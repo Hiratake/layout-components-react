67612ed0b8051902d9c9862b3f8497b2d3357f10
Hiratake <hiratake@hiratake.xyz>
2022-03-11 21:32:16 +0900

    build: ビルドの設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, './src/main.ts'),
+      name: 'Layout',
+      fileName: (format) => `layout.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    target: 'ESNext',
+  },
 })
